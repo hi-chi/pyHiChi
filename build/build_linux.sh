8518f598a868c8849b03cf4682787244d8efdd84
kozinove <evgeniy.kozinov@gmail.com>
2020-01-22 00:26:51 +0300

    added option for mkl in build_linux.sh

--- build/build_linux.sh
+++ build/build_linux.sh
@@ -54,6 +54,10 @@ case $key in
     USE_FFTW="ON"
     shift # past argument
     ;;
+    -mkl_fft)
+    USE_MKL="ON"
+    shift # past argument
+    ;;
     *)    # unknown option
     shift # past argument
     ;;
@@ -65,6 +69,9 @@ if [ $USE_OMP = "ON" ]; then
 fi
 if [ $USE_FFTW = "ON" ]; then
     CPU_OPTIONS="$CPU_OPTIONS -DUSE_FFTW=ON"
+fi
+if [ $USE_MKL = "ON" ]; then
+    CPU_OPTIONS="$CPU_OPTIONS -DUSE_MKL=ON"
 fi
 CPU_OPTIONS="$CPU_OPTIONS -DPYTHON_EXECUTABLE:FILEPATH=$python_path"
 
