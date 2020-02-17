2d2f8a022d9d47b8fb75ccdb4b18ad9dee113ee0
ValentinV95 <valyav95@mail.ru>
2020-02-17 21:34:31 +0300

    Fixed inconsistency type with python

--- src/Modules/Particle/include/Handler.h
+++ src/Modules/Particle/include/Handler.h
@@ -40,20 +40,20 @@ class BorisPusher
 {
 public:
 	template<class T_Particle>
-	void operator()(T_Particle& particle, ValueField field, FP timeStep)
+	void operator()(T_Particle* particle, ValueField field, FP timeStep)
 	{
 		ScalarBorisPusher scalarPusher;
-		handleChunk(scalarPusher, particle, field, timeStep);
+		handleChunk(scalarPusher, *particle, field, timeStep);
 	}
 
 	template<class T_ParticleArray>
-	void operator()(T_ParticleArray& particleArray, std::vector<ValueField> fields, FP timeStep)
+	void operator()(T_ParticleArray* particleArray, std::vector<ValueField> fields, FP timeStep)
 	{
 		typedef typename T_ParticleArray::ParticleProxyType ParticleProxyType;
 		ScalarBorisPusher scalarPusher;
-		for (int i = 0; i < particleArray.size(); i++)
+		for (int i = 0; i < particleArray->size(); i++)
 		{
-			ParticleProxyType particle = particleArray[i];
+			ParticleProxyType particle = (*particleArray)[i];
 			ValueField field = fields[i];
 			handleChunk(scalarPusher, particle, field, timeStep);
 		}
