953ad383001b3dfda018d75dec62464677361940
PanovaElena <alyona-gra98@yandex.ru>
2020-03-28 23:33:32 +0300

    returned script example_FDTD.py

--- example-tests/example_FDTD.py
+++ example-tests/example_FDTD.py
@@ -6,20 +6,20 @@ def valueEx(x, y, z):
     Ex = 0
     return Ex
 def valueEy(x, y, z):
-    Ey = np.cos(x + ma.pi/6)
+    Ey = np.cos(z + ma.pi/6)
     return Ey
 def valueEz(x, y, z):
     Ez = 0
     return Ez
 
 def valueBx(x, y, z):
-    Bx = 0
+    Bx = -np.cos(z+ma.pi/6)
     return Bx
 def valueBy(x, y, z):
     By = 0
     return By
 def valueBz(x, y, z):
-    Bz = np.cos(x + ma.pi/6)
+    Bz = 0
     return Bz
 
 def step(minCoords, maxCoords, gridSize):
@@ -48,11 +48,11 @@ import matplotlib.animation as animation
 
 N = 37
 x = np.arange(0, 3.5, 3.5/N)
-y = np.arange(1.0, 7.0, 6.0/N)
+z = np.arange(0, 2*ma.pi, 2*ma.pi/N)
 
 
 def getFields():
-    global grid, x, y, N
+    global grid, x, z, N
     Ex = np.zeros(shape=(N,N))
     Ey = np.zeros(shape=(N,N))
     Ez = np.zeros(shape=(N,N))
@@ -61,15 +61,15 @@ def getFields():
     Bz = np.zeros(shape=(N,N))
     for ix in range(N):
         for iy in range(N):
-            coordXY = pfc.vector3d(x[ix], y[iy], 0.0)
-            E = grid.getE(coordXY)
-            Ex[iy, ix] = E.x
-            Ey[iy, ix] = E.y
-            Ez[iy, ix] = E.z
-            B = grid.getB(coordXY)
-            Bx[iy, ix] = B.x
-            By[iy, ix] = B.y
-            Bz[iy, ix] = B.z
+            coordXZ = pfc.vector3d(x[ix], 0.0, z[iy])
+            E = grid.getE(coordXZ)
+            Ex[ix, iy] = E.x
+            Ey[ix, iy] = E.y
+            Ez[ix, iy] = E.z
+            B = grid.getB(coordXZ)
+            Bx[ix, iy] = B.x
+            By[ix, iy] = B.y
+            Bz[ix, iy] = B.z
     return Ex, Ey, Ez, Bx, By, Bz
 
 def updateData():
@@ -81,41 +81,41 @@ def updateData():
 
 fig, axes = plt.subplots(ncols=3, nrows=2)
 
-im11 = axes[0, 0].imshow(Ex, cmap='RdBu', interpolation='none', extent=(0, 3.5, 1.0, 7), animated = True)
+im11 = axes[0, 0].imshow(Ex, cmap='RdBu', interpolation='none', extent=(0, 2*ma.pi, 0, 3.5), animated = True)
 fig.colorbar(im11, ax=axes[0, 0])
 axes[0, 0].set_title("Ex")
 axes[0, 0].set_xlabel("x")
-axes[0, 0].set_ylabel("y")
+axes[0, 0].set_ylabel("z")
 
-im12 = axes[0, 1].imshow(Ey, cmap='RdBu', interpolation='none', extent=(0, 3.5, 1.0, 7), animated = True)
+im12 = axes[0, 1].imshow(Ey, cmap='RdBu', interpolation='none', extent=(0, 2*ma.pi, 0, 3.5), animated = True)
 fig.colorbar(im12, ax=axes[0, 1])
 axes[0, 1].set_title("Ey")
 axes[0, 1].set_xlabel("x")
-axes[0, 1].set_ylabel("y")
+axes[0, 1].set_ylabel("z")
 
-im13 = axes[0, 2].imshow(Ez, cmap='RdBu', interpolation='none', extent=(0, 3.5, 1.0, 7), animated = True)
+im13 = axes[0, 2].imshow(Ez, cmap='RdBu', interpolation='none', extent=(0, 2*ma.pi, 0, 3.5), animated = True)
 fig.colorbar(im13, ax=axes[0, 2])
 axes[0, 2].set_title("Ez")
 axes[0, 2].set_xlabel("x")
-axes[0, 2].set_ylabel("y")
+axes[0, 2].set_ylabel("z")
 
-im21 = axes[1, 0].imshow(Bx, cmap='RdBu', interpolation='none', extent=(0, 3.5, 1.0, 7), animated = True)
+im21 = axes[1, 0].imshow(Bx, cmap='RdBu', interpolation='none', extent=(0, 2*ma.pi, 0, 3.5), animated = True)
 fig.colorbar(im21, ax=axes[1, 0])
 axes[1, 0].set_title("Bx")
 axes[1, 0].set_xlabel("x")
-axes[1, 0].set_ylabel("y")
+axes[1, 0].set_ylabel("z")
 
-im22 = axes[1, 1].imshow(By, cmap='RdBu', interpolation='none', extent=(0, 3.5, 1.0, 7), animated = True)
+im22 = axes[1, 1].imshow(By, cmap='RdBu', interpolation='none', extent=(0, 2*ma.pi, 0, 3.5), animated = True)
 fig.colorbar(im22, ax=axes[1, 1])
 axes[1, 1].set_title("By")
 axes[1, 1].set_xlabel("x")
-axes[1, 1].set_ylabel("y")
+axes[1, 1].set_ylabel("z")
 
-im23 = axes[1, 2].imshow(Bz, cmap='RdBu', interpolation='none', extent=(0, 3.5, 1.0, 7), animated = True)
+im23 = axes[1, 2].imshow(Bz, cmap='RdBu', interpolation='none', extent=(0, 2*ma.pi, 0, 3.5), animated = True)
 fig.colorbar(im23, ax=axes[1, 2])
 axes[1, 2].set_title("Bz")
 axes[1, 2].set_xlabel("x")
-axes[1, 2].set_ylabel("y")
+axes[1, 2].set_ylabel("z")
 
 
 def updatefig(*args):
