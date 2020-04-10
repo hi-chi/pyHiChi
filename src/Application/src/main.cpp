dc9d8c67631db040873a89790f136fb1d88f55db
PanovaElena <alyona-gra98@yandex.ru>
2020-04-10 20:15:06 +0300

    added application to optimize

--- src/Application/src/main.cpp
+++ src/Application/src/main.cpp
@@ -1,9 +1,71 @@
-#include "TestingUtility.h"
+#include "pyGrid.h"
+
+#include "Constants.h"
+#include "Psatd.h"
+#include "Vectors.h"
+#include "Enums.h"
+#include "Mapping.h"
+#include "FieldConfiguration.h"
+
+#include <chrono>
+
+using namespace pfc;
+
+
+const int FACTOR = 1.0;
+const int NX_FULL = int(FACTOR * 320);
+const int NY = int(FACTOR * 256);
+const int NZ = int(FACTOR * 256);
+const int NX_BAND = int(56 * FACTOR);
+const FP3 GRID_SIZE(NX_BAND, NY, NZ);
+
+const FP WAVELENGTH = 1.0;
+const FP PULSELENGTH = 2.0 * WAVELENGTH;
+const FP PHASE = 0.0;
+const FP R0 = 16 * WAVELENGTH;
+const FP F_NUMBER = 0.3;
+const FP EDGE_SMOOTHING_ANGLE = 0.1;
+const FP OPENING_ANGLE = atan(1.0 / (2.0*F_NUMBER));
+const FP TOTAL_POWER = constants::c;
+
+const FP TIME_STEP = 8.0 / WAVELENGTH * constants::c;
+const int MAX_ITER = 4;
+
+const FP3 MIN_COORDS(-20.0, -20.0, -20.0);
+const FP3 MAX_COORDS(20.0, 20.0, 20.0);
+
+const FP D = 3.5 * PULSELENGTH;
+
 
 int main(int argc, char **argv)
 {
-    testing::InitGoogleTest(&argc, argv);
-    int result = RUN_ALL_TESTS();
+    TightFocusingField startConditions(F_NUMBER, R0, WAVELENGTH,
+        PULSELENGTH, PHASE, TOTAL_POWER, EDGE_SMOOTHING_ANGLE);
 
-    return result;
+    std::chrono::steady_clock::time_point startTimeInit = std::chrono::steady_clock::now();
+
+    TightFocusingMapping mapping(R0, PULSELENGTH, D);
+
+    FP xMin = mapping.getxMin();
+    FP xMax = mapping.getxMax();
+
+    FP3 gridMinCoords = FP3(xMin, MIN_COORDS.y, MIN_COORDS.z);
+    FP3 gridMaxCoords = FP3(xMax, MAX_COORDS.y, MAX_COORDS.z);
+    FP3 gridStep = (gridMaxCoords - gridMinCoords) / GRID_SIZE;
+
+    pyPSATDGridMapping grid(GRID_SIZE, TIME_STEP, gridMinCoords, gridStep);
+    grid.setMapping(&mapping);
+
+    PSATD fieldSolver(&grid);
+
+    grid.setFieldConfiguration<TightFocusingField>(&startConditions);
+    fieldSolver.convertFieldsPoissonEquation();
+
+    std::chrono::steady_clock::time_point endTimeInit = std::chrono::steady_clock::now();
+    std::chrono::seconds timeInit =
+        std::chrono::duration_cast<std::chrono::seconds>(endTimeInit - startTimeInit);
+
+    std::cout << "Initialisation time is " << timeInit.count() << " s" << std::endl;
+
+    return 0;
 }
