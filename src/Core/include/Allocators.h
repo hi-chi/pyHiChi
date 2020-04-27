2db6ab83f5c2826fa6b0c02bf8d4837226655d20
PanovaElena <alyona-gra98@yandex.ru>
2020-04-27 17:50:01 +0300

    added memset

--- src/Core/include/Allocators.h
+++ src/Core/include/Allocators.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <omp.h>
 
 namespace pfc {
 
@@ -22,12 +23,15 @@ namespace pfc {
         {
             const size_t size_vt = sizeof(value_type);
             const size_t len = num * size_vt;
+            const size_t num_threads = omp_get_max_threads();
+            const size_t block_size = (num + num_threads - 1) / num_threads * size_vt;
+            const size_t block_size_rem = len - block_size * (num_threads - 1);
             char * p = reinterpret_cast<char*>(std::malloc(len));
-#pragma omp parallel for simd
-            for (int i = 0; i < len; i += size_vt)
-#pragma unroll(16)
-                for (int j = 0; j < size_vt; j++)
-                    p[i + j] = 0;
+#pragma omp parallel for
+            for (int thr = 0; thr < num_threads; thr++) {
+                const int cur_block_size = thr == num_threads - 1 ? block_size_rem : block_size;
+                std::memset(p + thr * block_size, 0, cur_block_size);
+            }
             return reinterpret_cast<value_type*>(p);
         }
 
