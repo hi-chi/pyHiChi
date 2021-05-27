67cfbbb1c88d2fae0d7d32a6743f3f697d8a3e9f
AleksandrPanov <sasha1_1997@mail.ru>
2021-05-27 15:38:13 +0300

    save/load Simulation

--- src/Simulation/include/Simulation.h
+++ src/Simulation/include/Simulation.h
@@ -1,13 +1,38 @@
 #pragma once
 #include "Grid.h"
+#include "FieldEntity.h"
+
+#include <memory>
+
 namespace pfc {
 
+    template <class TGrid, class TFieldSolver>
     class Simulation {
     public:
-        Simulation()
-        {
+        std::shared_ptr<FieldEntity<TGrid, TFieldSolver>> field;
+        Simulation(const std::shared_ptr<FieldEntity<TGrid, TFieldSolver>>& field);
 
-        }
+        void save(std::ostream& ostr);
+        void load(std::istream& istr);
+
+    protected:
     };
-    
+
+    template<class TGrid, class TFieldSolver>
+    inline Simulation<TGrid, TFieldSolver>::Simulation(const std::shared_ptr<FieldEntity<TGrid, TFieldSolver>>& field)
+    {
+        this->field = field;
+    }
+
+    template<class TGrid, class TFieldSolver>
+    inline void Simulation<TGrid, TFieldSolver>::save(std::ostream& ostr)
+    {
+        field->save(ostr);
+    }
+
+    template<class TGrid, class TFieldSolver>
+    inline void Simulation<TGrid, TFieldSolver>::load(std::istream& istr)
+    {
+        field->load(istr);
+    }
 }
--- src/UnitTests/src/testSimulation.cpp
+++ src/UnitTests/src/testSimulation.cpp
@@ -1,7 +1,25 @@
+#include <sstream>
 #include "Simulation.h"
 #include "TestingUtility.h"
 
-TEST(SimulationTest, SimulationDefaultConstructor)
+#include "Grid.h"
+#include "Fdtd.h"
+#include "FieldEntity.h"
+
+
+TEST(SimulationTest, Simulation_save_load)
 {
-    ASSERT_NO_THROW(Simulation simulation());
+    std::shared_ptr<FieldEntity<YeeGrid, FDTD>> ptr1, ptr2;
+    FieldEntity<YeeGrid, FDTD>* fieldEntity = new FieldEntity<YeeGrid, FDTD>(Int3(8, 9, 10), FP3(0.0, 0.0, 0.0), FP3(1.0, 1.0, 1.0), 2e-12);
+    FieldEntity<YeeGrid, FDTD>* fieldTmp = new FieldEntity<YeeGrid, FDTD>(Int3(10, 7, 129), FP3(0.1, -0.2, 0.3), FP3(1.1, 1.2, 1.3), 4e-14);
+    ptr1.reset(fieldEntity);
+    ptr2.reset(fieldTmp);
+
+    Simulation<YeeGrid, FDTD> simulation(ptr1);
+    std::stringstream sstr;
+    simulation.save(sstr);
+    simulation = Simulation<YeeGrid, FDTD>(ptr2);
+    simulation.load(sstr);
+    ASSERT_EQ(fieldEntity->fieldSolver->dt, simulation.field->fieldSolver->dt);
+
 }
