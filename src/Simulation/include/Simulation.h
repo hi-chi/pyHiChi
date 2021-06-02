bfdcf682f97dc402b2aadc9754738242f3a6aaca
AleksandrPanov <sasha1_1997@mail.ru>
2021-06-02 18:59:09 +0300

    added first mpi

--- src/Simulation/include/Simulation.h
+++ src/Simulation/include/Simulation.h
@@ -37,11 +37,25 @@ namespace pfc {
             return Int3((MPI_rank % (domainSize.x * domainSize.y)) % domainSize.x, (MPI_rank % (domainSize.x * domainSize.y)) / domainSize.x, MPI_rank / (domainSize.x * domainSize.y));
         }
         bool isInside(Int3 indx) { return indx >= Int3(0, 0, 0) && indx < domainSize; }
+        void init() {};
 
-        virtual void init() {};
+        virtual void waitRecive(MPI_TAG tag) { isMPImessage = 1; } // need add tag
+        virtual bool isMessage(MPI_TAG tag) // need add tag
+        {
+            if (isMPImessage == 1)
+            {
+                isMPImessage = 0;
+                return true;
+            }
+            return false;
+        }
+
         virtual void iSend(char* ar, int size, MPI_TAG tag){}
-        virtual void iRecive(char* ar, int size, MPI_TAG tag){} // ????
+        virtual void iRecive(char* ar, int size, MPI_TAG tag) {}
+        virtual void setMessage(char* ar, int &size, MPI_TAG tag) {}
         virtual void finalize(){}
+    protected:
+        int isMPImessage = 0;
     };
 
     class NullDomain : public BaseDomain
@@ -52,7 +66,11 @@ namespace pfc {
     class SingleDomain : public BaseDomain
     {
     public:
-        void init() override
+        SingleDomain()
+        {
+            this->init();
+        }
+        void init()
         {
             for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
@@ -92,13 +110,11 @@ namespace pfc {
 
         Simulation(const std::shared_ptr<FieldEntity<TGrid, TFieldSolver>>& field) : field(field) {
             domain.reset(new SingleDomain());
-            domain->init(); // need fixed
         }
         Simulation(const std::shared_ptr<FieldEntity<TGrid, TFieldSolver>>& field,
                    const std::shared_ptr<Ensemble<TParticleArray>>& ensemble,
                    const std::shared_ptr<BorisPusher>& particlePusher): field(field), ensemble(ensemble), particlePusher(particlePusher) {
             domain.reset(new SingleDomain());
-            domain->init(); // need fixed
         }
 
         void save(std::ostream& ostr) override
@@ -111,6 +127,7 @@ namespace pfc {
             field->load(istr);
             if (ensemble) ensemble->load(istr);
         }
+        int sizeParts[3][3][3] = {0};
         void runIteration() override
         {
             field->getFieldSolver()->updateFields();
@@ -120,22 +137,45 @@ namespace pfc {
                 {
                     TParticleArray* particleArr = &(*ensemble)[i];
                     particlePusher->operator()(particleArr, field->getGrid(), field->getFieldSolver()->dt);
-                    std::vector<Particle3d> migratedParticles[3][3][3]; // need fixed
+                    std::vector<Particle3d> migratedParticles[3][3][3];
                     particleArr->cutMigratedParticles(migratedParticles, field->getGrid()->getMinInternalCoord(), field->getGrid()->getMaxInternalCoord());
                     for (int i = 0; i < 3; i++)
                     for (int j = 0; j < 3; j++)
                     for (int k = 0; k < 3; k++)
                     {
-                        if (!(i == 1 && j == 1 && k == 1))
+                        char* ar = (char*)&migratedParticles[i][j][k];
+                        const int size = migratedParticles[i][j][k].size() * sizeof(Particle3d);
+                        domain->neighbors[i][j][k]->iSend(ar, size, MPI_TAG::TAG_PARTICLE);
+                        domain->neighbors[i][j][k]->waitRecive(MPI_TAG::TAG_PARTICLE);
+                        sizeParts[i][j][k] += migratedParticles[i][j][k].size();
+                    }
+                    BaseDomain** tmpNeighbors = (BaseDomain**)domain->neighbors;
+                    int count = 0;
+                    while (count != 27)
+                    for (int i = 0; i < 3*3*3; i++)
+                    {
+                        if (tmpNeighbors[i]->isMessage(MPI_TAG::TAG_PARTICLE))
                         {
-                            char* ar = (char*)&migratedParticles[i][j][k];
-                            const int size = migratedParticles[i][j][k].size() * sizeof(Particle3d);
-                            domain->neighbors[i][j][k]->iSend(ar, size, MPI_TAG::TAG_PARTICLE);
+                            count++;
+                            int size = 0;
+                            char* ar = 0;
+                            tmpNeighbors[i]->setMessage(ar, size, MPI_TAG::TAG_PARTICLE);
+                            Particle3d* parts = (Particle3d*)ar;
+                            // copy ar to Ensemble
+                            for (int i = 0; i < size / sizeof(Particle3d); i++)
+                                particleArr->pushBack(*parts);
+                            if (ar) delete ar;
                         }
                     }
                 }
 
             }
+            std::cout << curIteration << " ";
+            for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+            for (int k = 0; k < 3; k++)
+                if (sizeParts[i][j][k] > 0) std::cout << Int3(i, j, k) << " " << sizeParts[i][j][k] << " ";
+            std::cout << "\n";
             curIteration++;
         }
         void run()
--- src/Simulation/src/Simulation.cpp
+++ src/Simulation/src/Simulation.cpp
@@ -5,12 +5,15 @@
 class MPIDomain : public BaseDomain
 {
 public:
+    MPI_Request req;
+    MPI_Status status;
     MPIDomain(int MPI_rank, int MPI_size, Int3 domainSize)
     {
         this->MPI_rank = MPI_rank;
         this->MPI_size = MPI_size;
         this->domainSize = domainSize;
-        this->domainIndx = this->getInt3Rank();;
+        this->domainIndx = this->getInt3Rank();
+        this->init();
     }
     MPIDomain(Int3 domainRank, int MPI_size, Int3 domainSize)
     {
@@ -18,8 +21,9 @@ public:
         this->domainSize = domainSize;
         this->domainIndx = domainRank;
         this->MPI_rank = this->getLinearRank();
+        this->init();
     }
-    void init() override
+    void init()
     {
         for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
@@ -34,6 +38,29 @@ public:
             }
         }
     }
+    void iSend(char* ar, int size, MPI_TAG tag) override
+    {
+        MPI_Isend(ar, size, MPI_CHAR, this->MPI_rank, tag, MPI_COMM_WORLD, &req);
+    }
+
+    void waitRecive(MPI_TAG tag) override {} // need add tag
+    bool isMessage(MPI_TAG tag) override // need add tag
+    {
+        MPI_Iprobe(MPI_rank, tag, MPI_COMM_WORLD, &isMPImessage, &status);
+        if (isMPImessage == 1)
+        {
+            isMPImessage = 0;
+
+            return true;
+        }
+        return false;
+    }
+    void setMessage(char* ar, int& size, MPI_TAG tag) override 
+    {
+        MPI_Get_count(&status, MPI_CHAR, &size);
+        ar = new char[size];
+        MPI_Recv(ar, size, MPI_CHAR, MPI_rank, tag, MPI_COMM_WORLD, &status);
+    }
 };
 namespace pfc {
     namespace ParticleInfo {
@@ -44,15 +71,46 @@ namespace pfc {
         const ParticleType* types = &ParticleInfo::typesVector[0];
         short numTypes = sizeParticleTypes;
     }
-}
-    int main(int arc, char* argv[])
+    FP urand(FP a, FP b) {
+        return a + (b - a) * ((FP)rand()) / RAND_MAX;
+    }
+    Particle3d randomParticle(FP3 minPosition = { 0.0, 0.0, 0.0 }, FP3 maxPosition = { 1.0, 1.0, 1.0 }, ParticleTypes type = ParticleTypes::Electron)
     {
-        std::shared_ptr<FieldEntity<YeeGrid, FDTD>> ptrField = std::make_shared<FieldEntity<YeeGrid, FDTD>>(Int3(8, 9, 10), FP3(0.0, 0.0, 0.0), FP3(1.0, 1.0, 1.0), 2e-12);
+        FP3 position;
+        for (int d = 0; d < 3; d++)
+            position[d] = urand(minPosition[d], maxPosition[d]);
+        Real minMomentum = -10.0;
+        Real maxMomentum =  10.0;
+        FP3 momentum(urand(minMomentum, maxMomentum),
+            urand(minMomentum, maxMomentum), urand(minMomentum, maxMomentum));
+        Real weight = static_cast<Real>(urand(1e-5, 1e5));
+        return Particle3d(position, momentum, weight, type);
+    }
+}
+    int main(int argc, char* argv[])
+    {
+        int size_x = 1, size_y = 1, mpi_size = 1, mpi_rank = 0;
+        if (argc > 1)
+        {
+            size_x = std::stoi(argv[1]);
+            size_y = std::stoi(argv[2]);
+            MPI_Init(&argc, &argv);
+            MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+            MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+            std::cout << mpi_size << " " << mpi_rank << "\n";
+
+        }
+        std::shared_ptr<FieldEntity<YeeGrid, FDTD>> ptrField = std::make_shared<FieldEntity<YeeGrid, FDTD>>(Int3(10, 10, 10), FP3(0.0, 0.0, 0.0), FP3(0.1, 0.1, 0.1), 2e-14);
         std::shared_ptr<Ensemble<ParticleArray3d>> ptrEnsemble = std::make_shared<Ensemble<ParticleArray3d>>();
         std::shared_ptr<BorisPusher> ptrPusher = std::make_shared<BorisPusher>();
 
-        //for (int i = 0; i < 9; i++)
-        //    ensemble->addParticle(this->randomParticle());
+
+
+        for (int i = 0; i < 10000; i++)
+            ptrEnsemble->addParticle(randomParticle());
         BaseSimulation* simulation = new Simulation<YeeGrid, FDTD, ParticleArray3d>(ptrField, ptrEnsemble, ptrPusher); //BaseSimulation* simulation = new Simulation<YeeGrid, FDTD>(ptrField);
+        for (int i = 0; i < 1; i++)
+            simulation->runIteration();
+        if (argc > 1) MPI_Finalize();
         return 0;
     }
