a60a8a8f285d0d2d635e8e19b5dba2158883cc08
Elena <alyona-gra98@yandex.ru>
2022-06-23 13:54:41 +0300

    try build tests again

--- src/UnitTests/src/testSaveLoadParticle.cpp
+++ src/UnitTests/src/testSaveLoadParticle.cpp
@@ -36,9 +36,9 @@ TYPED_TEST(SaveLoadParticle, testParticle)
 {
     using ParticleType = typename SaveLoadParticle<TypeParam>::Particle;
 
-    this->momentum = MomentumType(-231.3e9, 0.0, 1.23e-5);
-    this->position = getPosition(-0.06, 13e-5, -2e3);
-    this->weight = static_cast<WeightType>(1.4e2);
+    this->momentum = SaveLoadParticle<TypeParam>::MomentumType(-231.3e9, 0.0, 1.23e-5);
+    this->position = this->getPosition(-0.06, 13e-5, -2e3);
+    this->weight = static_cast<SaveLoadParticle<TypeParam>::WeightType>(1.4e2);
     this->type = ParticleTypes::Proton;
     this->particle = ParticleType(this->position, this->momentum, this->weight, this->type);
     
@@ -86,8 +86,8 @@ TYPED_TEST(ParticleArrayTest, testParticlesSaveLoad)
 
 TYPED_TEST(ParticleArrayTest, testParticlesSaveLoadProcessingWrongType)
 {
-    typedef typename ParticleArrayAoS<One> WrongParticleArrayType;
-    typedef typename ParticleArrayTest<TypeParam>::ParticleArray CorrectParticleArrayType;
+    using WrongParticleArrayType = ParticleArrayAoS<One>;
+    using CorrectParticleArrayType = typename ParticleArrayTest<TypeParam>::ParticleArray;
 
     CorrectParticleArrayType particles;
     WrongParticleArrayType res;
